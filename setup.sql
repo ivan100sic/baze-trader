894f4fdfad22109d03cf6de36b296742da6ee39e
Ivan Stosic <ivan100sic@gmail.com>
2018-02-12 22:40:11 +0100

    Radio jos malo

--- setup.sql
+++ setup.sql
@@ -11,32 +11,13 @@ create table user (
 	unique index (user_email)
 );
 
-/* tabela koja ce cuvati informaciju o trenutnoj
-tarifi u svom jedinom redu */
-create table fee (
-	fee double not null
-);
-
-/* odmah ubacujemo default vrednost */
-insert into fee(fee) values (0.0016);
-
-/* procedura za postavljanje fee-a */
-delimiter //
-create procedure set_fee(in new_fee double)
-begin
-	update fee set fee = new_fee;
-end //
-delimiter ;
+/* nema fee-ova, kod nas je sve dzabe! */
 
 /* 
 	Valute dostupne za trading.
-	
-	Ogranicavamo da currency code mora da se 
-	sastoji od tri slova
 */
 create table currency (
-	currency_code varchar(3) primary key not null
-		check (currency_code rlike "^[A-Z]+$"),
+	currency_code varchar(3) primary key not null,
 
 	currency_name varchar(45) not null
 );
@@ -71,21 +52,116 @@ create table wallet (
 create table trade (
 	trade_id int primary key not null auto_increment,
 	
-	wallet_id int not null,
+	wallet_id_from int not null,
+	wallet_id_to int not null,
 
-	foreign key (wallet_id)
+	foreign key (wallet_id_from)
 		references wallet(wallet_id)
 		on update cascade
 		on delete restrict,
 
-	trade_currency_code varchar(3) not null,
-
-	foreign key (trade_currency_code)
-		references currency(currency_code)
+	foreign key (wallet_id_to)
+		references wallet(wallet_id)
 		on update cascade
 		on delete restrict,
 
-	amount double not null,
-	ratio double not null
+	trade_amount double not null,
+	trade_ratio double not null,
+
+	trade_created_date datetime not null,
+	trade_completed_date datetime
+	# null completed date znaci nije completed
 );
 
+delimiter //
+create function get_base_currency(trade_id int)
+returns varchar(3)
+begin
+	return (select currency_code from wallet, trade
+		where wallet.wallet_id = trade.wallet_id);
+end //
+delimiter ;
+
+/* procedura za kreditovanje wallet-a */
+delimiter //
+create procedure credit_wallet(w_id int, delta double)
+begin
+	update wallet set amount = amount + delta
+		where wallet_id = w_id;
+end //
+delimiter ;
+
+delimiter //
+create trigger trade_trigger
+after insert on trade
+for each row
+begin
+	declare break_condition int;
+	
+	declare base_currency varchar(3);
+
+	declare this_ratio double;
+	declare that_ratio double;
+	declare this_amount double;
+	declare that_amount double;
+
+	declare mean_ratio double;
+
+	declare that_wallet int;
+
+	set break_condition = 0;
+	set base_currency = get_base_currency(new.trade_id);
+
+	# pronadji najbolji offer sa druge strane, ako postoji
+	set @rows = (select count(*) from trade where
+		trade_currency_code = @base_currency
+		and
+		get_base_currency(trade_id)
+			= new.trade_currency_code
+		and
+		trade_completed_date is null
+	);
+
+	if @rows > 0
+	then
+		set @row_id = 
+		(
+			select min(trade_id) from trade where
+				trade_currency_code = @base_currency
+				and
+				get_base_currency(trade_id)
+					= new.trade_currency_code
+				and
+				trade_completed_date is null
+			order by 
+				trade_ratio desc,
+				trade_created_date asc
+		);
+
+		
+
+		set this_ratio = new.trade_ratio;
+		set that_ratio = (select trade_ratio
+			from trade where trade_id = @row_id);
+
+		set this_amount = new.trade_amount;
+		set that_amount = (select trade_amount
+			from trade where trade_id = @row_id);
+
+		set mean_ratio = sqrt(this_ratio * that_ratio);
+
+		# ukoliko mogu da se upare, upari ih
+		if this_ratio * that_ratio >= 1.0
+		then
+			if this_amount > that_amount
+			then
+				# this prezivljava, nije potpuno sparen
+				credit_wallet(new.wallet_id, that_amount)
+
+			end if;
+
+		end if;
+
+	end if;
+end //
+delimiter ;
